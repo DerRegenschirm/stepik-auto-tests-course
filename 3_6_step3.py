35207dfb017f1c57f7fa818afdcb79fe756bc204
DerRegenschirm <obuhovavohubo@gmail.com>
2021-01-10 19:45:08 +0200

    with comments

--- 3_6_step3.py
+++ 3_6_step3.py
@@ -3,9 +3,6 @@ import time
 import math
 
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 
 @pytest.fixture()
 def browser():
@@ -26,24 +23,23 @@ def test_links(browser, link):
     browser.get(link)
 
     browser.implicitly_wait(5)
+#we need to wait for 5s for each element
 
-    #input_area = WebDriverWait(browser, 5).until(
-    #    EC.visibility_of((By.CLASS_NAME,"ember-text-area"))
-    #)
-
-    input_area=browser.find_element_by_css_selector(".ember-text-area")
+    input_area = browser.find_element_by_css_selector(".ember-text-area")
+#find the imput box
 
     answer = calc()
     input_area.send_keys(answer)
+#calculater an answer
 
     btn = browser.find_element_by_css_selector("button.submit-submission")
     btn.click()
+#send the answer
 
-    #output_area = WebDriverWait(browser, 5).until(
-    #    EC.visibility_of((By.CLASS_NAME, "smart-hints__feedback"))
-    #)
     output_area = browser.find_element_by_css_selector(".smart-hints__feedback")
     output_text = output_area.text
+#find a new feedback box
 
     assert output_text == "Correct!", f"other text - {output_text}"
+#compare the text
 
