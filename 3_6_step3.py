18505fa0f4f6e0ca52ebd2b38a463c48c33f4a03
DerRegenschirm <obuhovavohubo@gmail.com>
2021-01-10 19:23:39 +0200

    1 test

--- 3_6_step3.py
+++ 3_6_step3.py
@@ -15,9 +15,11 @@ def browser():
     print("\nquit browser..")
     browser.quit()
 
-link="https://stepik.org/lesson/236895/step/1"
+
 #@pytest.mark.parametrize('link', ["https://stepik.org/lesson/236895/step/1","https://stepik.org/lesson/236896/step/1","https://stepik.org/lesson/236897/step/1","https://stepik.org/lesson/236898/step/1","https://stepik.org/lesson/236899/step/1","https://stepik.org/lesson/236903/step/1","https://stepik.org/lesson/236904/step/1","https://stepik.org/lesson/236905/step/1"])
-def test_links(browser, link):
+#def test_links(browser, link):
+def test_links(browser):
+    link = "https://stepik.org/lesson/236895/step/1"
     print(f"start test {link}")
     browser.get(link)
 
